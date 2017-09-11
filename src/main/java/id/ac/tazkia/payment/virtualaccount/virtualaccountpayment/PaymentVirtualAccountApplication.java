a18183c7053c8657c23f90004dc827f156b94eef
Endy Muhardin <endy.muhardin@gmail.com>
2017-09-11 13:27:28 +0700

    starter project

---
+++ src/main/java/id/ac/tazkia/payment/virtualaccount/virtualaccountpayment/PaymentVirtualAccountApplication.java
@@ -0,0 +1,12 @@
+package id.ac.tazkia.payment.virtualaccount.virtualaccountpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentVirtualAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentVirtualAccountApplication.class, args);
+	}
+}
