baf71f0a1aef76201fd8f2878327310ca5f83278
Endy Muhardin <endy.muhardin@gmail.com>
2017-09-24 12:19:11 +0700

    implement tampilan daftar bank

--- src/main/java/id/ac/tazkia/payment/virtualaccount/controller/BankController.java
+++ src/main/java/id/ac/tazkia/payment/virtualaccount/controller/BankController.java
@@ -5,19 +5,27 @@ import id.ac.tazkia.payment.virtualaccount.entity.Bank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
-@Transactional @RestController
-@RequestMapping("/api/bank")
+@Transactional @Controller
 public class BankController {
     @Autowired private BankDao bankDao;
 
-    @GetMapping("/")
+    @GetMapping("/api/bank/")
+    @ResponseBody
     public Page<Bank> findAll(Pageable page){
         return bankDao.findAll(page);
+    }
+
+    @GetMapping("/bank/list")
+    public ModelMap listBank(Pageable page){
+        return new ModelMap()
+                .addAttribute("daftarBank", bankDao.findAll(page))
+                .addAttribute("pageTitle", "Daftar Bank");
     }
 }
