ef98498d0653161bb7011ce712ce149fa6ecbea5
Endy Muhardin <endy.muhardin@gmail.com>
2017-09-28 15:42:51 +0700

    implement edit jenis tagihan

--- src/main/java/id/ac/tazkia/payment/virtualaccount/controller/JenisTagihanController.java
+++ src/main/java/id/ac/tazkia/payment/virtualaccount/controller/JenisTagihanController.java
@@ -8,21 +8,54 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.support.SessionStatus;
 
-@Transactional @Controller
+import javax.validation.Valid;
+
+@Transactional
+@Controller
 public class JenisTagihanController {
-    @Autowired private JenisTagihanDao jenisTagihanDao;
+    @Autowired
+    private JenisTagihanDao jenisTagihanDao;
 
     @GetMapping("/api/jenistagihan/")
-    public Page<JenisTagihan> findAll(Pageable page){
+    public Page<JenisTagihan> findAll(Pageable page) {
         return jenisTagihanDao.findAll(page);
     }
 
+    @ModelAttribute("pageTitle")
+    public String pageTitle() {
+        return "Jenis Tagihan";
+    }
+
     @GetMapping("/jenistagihan/list")
-    public ModelMap findAllHtml(){
+    public ModelMap findAllHtml() {
         return new ModelMap()
-                .addAttribute("pageTitle", "Jenis Tagihan")
                 .addAttribute("daftarJenisTagihan", jenisTagihanDao.findAll());
+    }
+
+    @GetMapping("/jenistagihan/form")
+    public ModelMap tampilkanForm(@RequestParam(value = "id", required = false) JenisTagihan jenisTagihan) {
+        if (jenisTagihan == null) {
+            jenisTagihan = new JenisTagihan();
+        }
+
+        return new ModelMap().addAttribute("jenisTagihan", jenisTagihan);
+    }
+
+    @PostMapping("/jenistagihan/form")
+    public String prosesForm(@ModelAttribute @Valid JenisTagihan jenisTagihan, BindingResult errors, SessionStatus status) {
+        if (errors.hasErrors()) {
+            return "/jenistagihan/form";
+        }
+
+        jenisTagihanDao.save(jenisTagihan);
+        status.setComplete();
+        return "redirect:list";
     }
 }
--- src/main/java/id/ac/tazkia/payment/virtualaccount/entity/TipePembayaran.java
+++ src/main/java/id/ac/tazkia/payment/virtualaccount/entity/TipePembayaran.java
@@ -1,7 +1,17 @@
 package id.ac.tazkia.payment.virtualaccount.entity;
 
 public enum TipePembayaran {
-    OPEN_PAYMENT,
-    CLOSED_PAYMENT,
-    INSTALLMENT
+    OPEN_PAYMENT("Jumlah Bebas"),
+    CLOSED_PAYMENT("Jumlah Tertentu"),
+    INSTALLMENT("Cicilan");
+
+    private final String label;
+
+    TipePembayaran(String label){
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return this.label;
+    }
 }
