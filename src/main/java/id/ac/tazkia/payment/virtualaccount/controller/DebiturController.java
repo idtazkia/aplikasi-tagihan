e053f27fddbc1944288a2eb3710fc4f7c5d86ba4
riski galang saputra <riskigalangsaputra@gmail.com>
2018-01-11 15:10:50 +0700

    crud debitur

--- src/main/java/id/ac/tazkia/payment/virtualaccount/controller/DebiturController.java
+++ src/main/java/id/ac/tazkia/payment/virtualaccount/controller/DebiturController.java
@@ -8,66 +8,86 @@ import id.ac.tazkia.payment.virtualaccount.entity.Tagihan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
 import java.util.Arrays;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.support.SessionStatus;
 
-@Transactional @Controller
+@Transactional
+@Controller
 public class DebiturController {
-    @Autowired private DebiturDao debiturDao;
-    @Autowired private TagihanDao tagihanDao;
+
+    @Autowired
+    private DebiturDao debiturDao;
+    
+    @Autowired
+    private TagihanDao tagihanDao;
 
     @PreAuthorize("hasAuthority('VIEW_DEBITUR')")
-    @GetMapping("/api/client/debitur/") @ResponseBody
-    public Page<Debitur> findAll(Pageable page){
+    @GetMapping("/api/client/debitur/")
+    @ResponseBody
+    public Page<Debitur> findAll(Pageable page) {
         return debiturDao.findAll(page);
     }
 
     @PreAuthorize("hasAuthority('EDIT_DEBITUR')")
-    @PostMapping("/api/client/debitur/") @ResponseStatus(HttpStatus.CREATED)
-    public void create(@RequestBody @Valid Debitur s){
+    @PostMapping("/api/client/debitur/")
+    public String create(@ModelAttribute @Valid Debitur s) {
         debiturDao.save(s);
+
+        return "redirect:/debitur/list";
     }
 
     @PreAuthorize("hasAuthority('VIEW_TAGIHAN')")
-    @GetMapping("/api/client/debitur/{id}/tagihan") @ResponseBody
-    public Page<Tagihan> findTagihanOutstandingByDebitur(Debitur s, Pageable page){
+    @GetMapping("/api/client/debitur/{id}/tagihan")
+    @ResponseBody
+    public Page<Tagihan> findTagihanOutstandingByDebitur(Debitur s, Pageable page) {
         return tagihanDao.findByDebiturAndStatusPembayaranInOrderByUpdatedAtDesc(s,
                 Arrays.asList(StatusPembayaran.BELUM_DIBAYAR, StatusPembayaran.DIBAYAR_SEBAGIAN), page);
     }
 
     @PreAuthorize("hasAuthority('VIEW_DEBITUR')")
-    @GetMapping("/api/debitur/") @ResponseBody
-    public Page<Debitur> findAll(String search, Pageable page){
+    @GetMapping("/api/debitur/")
+    @ResponseBody
+    public Page<Debitur> findAll(String search, Pageable page) {
         return debiturDao.findByNomorDebiturOrNamaContainingIgnoreCase(search, search, page);
     }
 
     @PreAuthorize("hasAuthority('VIEW_DEBITUR')")
     @GetMapping("/debitur/list")
-    public String daftarDebitur(ModelMap mm){ 
+    public String daftarDebitur(ModelMap mm) {
         mm.addAttribute("data", debiturDao.findAll());
         return "debitur/list";
     }
 
     @ModelAttribute("pageTitle")
-    public String pageTitle(){
+    public String pageTitle() {
         return "Data Debitur";
+    }
+
+    @GetMapping("/debitur/forminput")
+    public String tampil(ModelMap mm) {
+        mm.addAttribute("debitur", new Debitur());
+        return "debitur/form";
     }
 
     @PreAuthorize("hasAuthority('EDIT_DEBITUR')")
     @GetMapping("/debitur/form")
-    public ModelMap tampilkanForm(@RequestParam(value = "id", required = false) Debitur debitur){
-        if(debitur == null){
+    public ModelMap tampilkanForm(@RequestParam(value = "id", required = false) String id) {
+        Debitur debitur;
+
+        if (id == null) {
             debitur = new Debitur();
+        } else {
+            debitur = debiturDao.findOne(id);
         }
+
         return new ModelMap("debitur", debitur);
     }
 
@@ -82,4 +102,15 @@ public class DebiturController {
         status.setComplete();
         return "redirect:list";
     }
+    
+    @PreAuthorize("hasAuthority('EDIT_DEBITUR')")
+    @GetMapping("/debitur/delete{id}")
+    public String hapusData(@RequestParam(value = "id", required = false) String id){
+        debiturDao.delete(id);
+        
+        return "redirect:/debitur/list";
+        
+    }
+    
+
 }
--- src/main/java/id/ac/tazkia/payment/virtualaccount/entity/Debitur.java
+++ src/main/java/id/ac/tazkia/payment/virtualaccount/entity/Debitur.java
@@ -9,9 +9,10 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
-@Entity @Data
+@Entity @Table
 public class Debitur {
     @Id @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "uuid2")
@@ -27,4 +28,46 @@ public class Debitur {
     @Email
     private String email;
     private String noHp;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNomorDebitur() {
+        return nomorDebitur;
+    }
+
+    public void setNomorDebitur(String nomorDebitur) {
+        this.nomorDebitur = nomorDebitur;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNoHp() {
+        return noHp;
+    }
+
+    public void setNoHp(String noHp) {
+        this.noHp = noHp;
+    }
+    
+    
 }
