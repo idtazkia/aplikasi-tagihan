09281c664e5b0d51de8a849f89dfcdcb5d4a24a7
Endy Muhardin <endy.muhardin@gmail.com>
2018-01-11 19:19:20 +0700

    replace getter/setter dengan lombok

--- src/main/java/id/ac/tazkia/payment/virtualaccount/entity/Bank.java
+++ src/main/java/id/ac/tazkia/payment/virtualaccount/entity/Bank.java
@@ -1,5 +1,6 @@
 package id.ac.tazkia.payment.virtualaccount.entity;
 
+import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.validator.constraints.NotEmpty;
 
@@ -9,7 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
-@Entity
+@Entity @Data
 public class Bank {
     @Id @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "uuid2")
@@ -26,52 +27,4 @@ public class Bank {
 
     @NotNull
     private Boolean aktif = Boolean.TRUE;
-
-    public Boolean getAktif() {
-        return aktif;
-    }
-
-    public void setAktif(Boolean aktif) {
-        this.aktif = aktif;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getKode() {
-        return kode;
-    }
-
-    public void setKode(String kode) {
-        this.kode = kode;
-    }
-
-    public String getNama() {
-        return nama;
-    }
-
-    public void setNama(String nama) {
-        this.nama = nama;
-    }
-
-    public String getNomorRekening() {
-        return nomorRekening;
-    }
-
-    public void setNomorRekening(String nomorRekening) {
-        this.nomorRekening = nomorRekening;
-    }
-
-    public String getNamaRekening() {
-        return namaRekening;
-    }
-
-    public void setNamaRekening(String namaRekening) {
-        this.namaRekening = namaRekening;
-    }
 }
