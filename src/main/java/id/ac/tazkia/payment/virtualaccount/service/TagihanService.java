ea0e7daffccc7f146271ca5ddeaffaa20c421aef
Endy Muhardin <endy.muhardin@gmail.com>
2018-01-11 22:50:13 +0700

    fix input tagihan

--- src/main/java/id/ac/tazkia/payment/virtualaccount/service/TagihanService.java
+++ src/main/java/id/ac/tazkia/payment/virtualaccount/service/TagihanService.java
@@ -6,18 +6,22 @@ import id.ac.tazkia.payment.virtualaccount.dao.VirtualAccountDao;
 import id.ac.tazkia.payment.virtualaccount.entity.Bank;
 import id.ac.tazkia.payment.virtualaccount.entity.Tagihan;
 import id.ac.tazkia.payment.virtualaccount.entity.VirtualAccount;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service @Transactional
 public class TagihanService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TagihanService.class);
 
     @Autowired private TagihanDao tagihanDao;
     @Autowired private VirtualAccountDao virtualAccountDao;
     @Autowired private BankDao bankDao;
 
     public void createTagihan(Tagihan t) {
+        LOGGER.debug("ID Tagihan : "+t.getId());
         tagihanDao.save(t);
         for (Bank b : bankDao.findAll()) {
             if (b.getAktif()) {
