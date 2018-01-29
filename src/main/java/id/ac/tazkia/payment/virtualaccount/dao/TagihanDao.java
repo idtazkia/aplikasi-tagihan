246d4e42b52032ed9669a75fd9a6b44d074c84a8
Endy Muhardin <endy.muhardin@gmail.com>
2018-01-29 10:47:03 +0700

    remove yang tidak dipakai

--- src/main/java/id/ac/tazkia/payment/virtualaccount/dao/TagihanDao.java
+++ src/main/java/id/ac/tazkia/payment/virtualaccount/dao/TagihanDao.java
@@ -5,10 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-import java.util.Collection;
-
 public interface TagihanDao extends PagingAndSortingRepository<Tagihan, String> {
-    Page<Tagihan> findByDebiturAndStatusPembayaranInOrderByUpdatedAtDesc(Debitur d, Collection<StatusPembayaran> status, Pageable pageable);
     Tagihan findByNomor(String nomor);
     Page<Tagihan> findAllByStatusTagihan(StatusTagihan status, Pageable pageable);
     Iterable<Tagihan> findByStatusNotifikasi(StatusNotifikasi statusNotifikasi);
