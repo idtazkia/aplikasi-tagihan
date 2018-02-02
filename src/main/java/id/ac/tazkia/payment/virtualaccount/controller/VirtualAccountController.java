85d8d92ae54d56e2f227ef361f900ff71881a487
Endy Muhardin <endy.muhardin@gmail.com>
2018-02-02 21:55:42 +0700

    tombol retry create va

--- src/main/java/id/ac/tazkia/payment/virtualaccount/controller/VirtualAccountController.java
+++ src/main/java/id/ac/tazkia/payment/virtualaccount/controller/VirtualAccountController.java
@@ -2,11 +2,14 @@ package id.ac.tazkia.payment.virtualaccount.controller;
 
 import id.ac.tazkia.payment.virtualaccount.dao.VirtualAccountDao;
 import id.ac.tazkia.payment.virtualaccount.entity.Tagihan;
+import id.ac.tazkia.payment.virtualaccount.entity.VaStatus;
+import id.ac.tazkia.payment.virtualaccount.entity.VirtualAccount;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -21,5 +24,15 @@ public class VirtualAccountController {
         return new ModelMap()
                 .addAttribute("listVa",
                         virtualAccountDao.findByTagihan(tagihan, page));
+    }
+
+    @PostMapping("/retry")
+    public String retryCreateVa(@RequestParam(name = "id") VirtualAccount virtualAccount) {
+        if (virtualAccount == null) {
+            return "redirect:/home";
+        }
+        virtualAccount.setVaStatus(VaStatus.CREATE);
+        virtualAccountDao.save(virtualAccount);
+        return "redirect:/va/list?tagihan=" + virtualAccount.getTagihan().getId();
     }
 }
