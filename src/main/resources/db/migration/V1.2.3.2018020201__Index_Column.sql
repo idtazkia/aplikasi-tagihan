f78cc92955da458334f52ff67944aa14d7fc3958
Endy Muhardin <endy.muhardin@gmail.com>
2018-02-02 16:34:12 +0700

    optimasi tabel tagihan

---
+++ src/main/resources/db/migration/V1.2.3.2018020201__Index_Column.sql
@@ -0,0 +1,3 @@
+create index tagihan_status_idx on tagihan(status_tagihan);
+create index tagihan_status_notifikasi_idx on tagihan(status_notifikasi);
+create index tagihan_tanggal_idx on tagihan(tanggal_tagihan);
