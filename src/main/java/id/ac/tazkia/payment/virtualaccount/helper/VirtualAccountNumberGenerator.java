a8e9e701ccba00a986717d56eef7b7e5a4f34370
Endy Muhardin <endy.muhardin@gmail.com>
2018-07-26 16:30:04 +0700

    fix string padding

--- src/main/java/id/ac/tazkia/payment/virtualaccount/helper/VirtualAccountNumberGenerator.java
+++ src/main/java/id/ac/tazkia/payment/virtualaccount/helper/VirtualAccountNumberGenerator.java
@@ -1,10 +1,14 @@
 package id.ac.tazkia.payment.virtualaccount.helper;
 
+import org.apache.commons.lang3.StringUtils;
+
 public abstract class VirtualAccountNumberGenerator {
     public static String generateVirtualAccountNumber(String nomor, Integer jumlahDigit) {
+        if(!StringUtils.isNumeric(nomor)){
+            throw new IllegalArgumentException("Nomor VA ["+nomor+"] tidak numerik");
+        }
         if (nomor.length() < jumlahDigit) {
-            return String.format("%-"+jumlahDigit+"s", nomor )
-                    .replace(' ', '0');
+            return StringUtils.rightPad(nomor, jumlahDigit, "0");
         } else {
             return nomor.substring(nomor.length() - jumlahDigit);
         }
