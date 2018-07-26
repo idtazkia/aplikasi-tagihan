c5b298287efca0544196dad45d4422ed049eff54
Endy Muhardin <endy.muhardin@gmail.com>
2018-07-26 15:23:10 +0700

    fix sonar warning

--- src/main/java/id/ac/tazkia/payment/virtualaccount/helper/VirtualAccountNumberGenerator.java
+++ src/main/java/id/ac/tazkia/payment/virtualaccount/helper/VirtualAccountNumberGenerator.java
@@ -1,6 +1,6 @@
 package id.ac.tazkia.payment.virtualaccount.helper;
 
-public class VirtualAccountNumberGenerator {
+public abstract class VirtualAccountNumberGenerator {
     public static String generateVirtualAccountNumber(String nomor, Integer jumlahDigit) {
         if (nomor.length() < jumlahDigit) {
             return String.format("%-"+jumlahDigit+"s", nomor )
